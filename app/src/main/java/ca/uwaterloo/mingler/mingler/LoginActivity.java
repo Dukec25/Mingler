a00c3df868f1b71dc5c8fd920f54cbc92e2aee2e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-09-17 13:56:48 -0400

    Add login page and Firebase libraries

---
+++ app/src/main/java/ca/uwaterloo/mingler/mingler/LoginActivity.java
@@ -0,0 +1,42 @@
+package ca.uwaterloo.mingler.mingler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by zhuowei on 2016-09-17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_login);
+        doAuth(null);
+    }
+    public void doAuth(View v) {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setProviders(AuthUI.EMAIL_PROVIDER, AuthUI.GOOGLE_PROVIDER)
+                .build();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                doAuth(null);
+            }
+        }
+    }
+}
--- app/src/main/java/ca/uwaterloo/mingler/mingler/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/mingler/mingler/MainActivity.java
@@ -1,7 +1,14 @@
 package ca.uwaterloo.mingler.mingler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(R.string.sign_out);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+        return true;
     }
 }
